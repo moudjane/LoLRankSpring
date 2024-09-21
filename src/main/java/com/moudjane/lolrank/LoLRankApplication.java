cd9307287a07f2365cc70d6d3b2ead3746741d18
moudjane <mathis.oudjane@gmail.com>
2024-09-21 11:18:05 +0200

    first commit

---
+++ src/main/java/com/moudjane/lolrank/LoLRankApplication.java
@@ -0,0 +1,13 @@
+package com.moudjane.lolrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoLRankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoLRankApplication.class, args);
+    }
+
+}
